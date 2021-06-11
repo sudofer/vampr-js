c557a23cd2bd37c89aa2b269de0ba17a02b65190
Andres Fernandez-Murray <andres.fmurray@gmail.com>
2021-06-11 22:53:08 +0000

    getting ready for traversal

--- vampire.js
+++ vampire.js
@@ -10,23 +10,43 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfPeople = 0;
+    let currentVampire = this;
+    //climb up tree
+    while (currentVampire.creator){
+      currentVampire = currentVampire.creator;
+      numberOfPeople++;
+    } 
+    return numberOfPeople;
   }
+  
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
+
+  get ListofDecendants() {
+    const decendants = [];
+    let currentVampire = this;
+    while (currentVampire.creator){
+      decendants.push(currentVampire.creator);
+      currentVampire = currentVampire.creator;
+    } 
+    return decendants;
+  }
+
 
   /** Stretch **/
 
@@ -36,7 +56,25 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    
+    let thisDecen = this.ListofDecendants;
+    let vampDecen = vampire.ListofDecendants;
 
+    thisDecen.unshift(this);
+    vampDecen.unshift(vampire);
+    
+    vampDecen = vampDecen.reverse();
+    thisDecen = thisDecen.reverse();
+    
+    const shortestList = Math.min(vampDecen.length,
+      thisDecen.length)
+
+    for (let i = 0; i < shortestList; i++) {
+      if (thisDecen[i] !== vampDecen[i]){
+        return thisDecen[i-1]; // case 1
+      }
+    } 
+    return thisDecen[shortestList-1] // case 2
   }
 }
 
